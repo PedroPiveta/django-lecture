0029e70055c849da31cf9052527650792e9dcec0
PedroPiveta <ppivetabarrotti208@gmail.com>
2022-12-14 12:49:12 -0300

    adding html

--- hello/views.py
+++ hello/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello!")
+    return render(request, "hello/index.html")
 
 def brian(request):
     return HttpResponse("Hello, Pedro!")
@@ -11,5 +11,7 @@ def brian(request):
 def david(request):
     return HttpResponse("Hello, David!")
 
-def greet(request,name):
-    return HttpResponse(f"Hello, {name}!")
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
